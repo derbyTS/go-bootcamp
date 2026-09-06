6be973f07c184437faa3684744940ad778351c75
derbyTS <derbysalvacion@gmail.com>
2026-09-07 03:14:25 +0800

    [WIP]middleware: Done with hpp

--- rest-api-project/REST_API_GO/cmd/api/server.go
+++ rest-api-project/REST_API_GO/cmd/api/server.go
@@ -19,88 +19,74 @@ type user struct {
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello Root Route"))
-	fmt.Println("Hello Root Route")
 }
 
 func teachersHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
 		w.Write([]byte("Hello Get Teachers Route"))
-		fmt.Println("Hello Get Teachers Route")
 		return
 	case http.MethodPost:
 		w.Write([]byte("Hello Post Teachers Route"))
-		fmt.Println("Hello Post Teachers Route")
 		return
 	case http.MethodPut:
 		w.Write([]byte("Hello Put Teachers Route"))
-		fmt.Println("Hello Put Teachers Route")
 		return
 	case http.MethodPatch:
 		w.Write([]byte("Hello Patch Teachers Route"))
-		fmt.Println("Hello Patch Teachers Route")
 		return
 	case http.MethodDelete:
 		w.Write([]byte("Hello Delete Teachers Route"))
-		fmt.Println("Hello Delete Teachers Route")
 	}
-
-	w.Write([]byte("Hello Default Teachers Route"))
-	fmt.Println("Hello Default Teachers Route")
 }
 
 func studentsHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
 		w.Write([]byte("Hello Get Students Route"))
-		fmt.Println("Hello Get Students Route")
 		return
 	case http.MethodPost:
 		w.Write([]byte("Hello Post Students Route"))
-		fmt.Println("Hello Post Students Route")
 		return
 	case http.MethodPut:
 		w.Write([]byte("Hello Put Students Route"))
-		fmt.Println("Hello Put Students Route")
 		return
 	case http.MethodPatch:
 		w.Write([]byte("Hello Patch Students Route"))
-		fmt.Println("Hello Patch Students Route")
 		return
 	case http.MethodDelete:
 		w.Write([]byte("Hello Delete Students Route"))
-		fmt.Println("Hello Delete Students Route")
 		return
 	}
-	w.Write([]byte("Hello Students Route"))
-	fmt.Println("Hello Students Route")
 }
 
 func execsHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
 		w.Write([]byte("Hello Get Execs Route"))
-		fmt.Println("Hello Get Execs Route")
 		return
 	case http.MethodPost:
+		fmt.Println("Query: ", r.URL.Query())
+		fmt.Println("name: ", r.URL.Query().Get("name"))
+
+		err := r.ParseForm()
+		if err != nil {
+			return
+		}
+		fmt.Println("Form from post method: ", r.Form)
+
 		w.Write([]byte("Hello Post Execs Route"))
-		fmt.Println("Hello Post Execs Route")
 		return
 	case http.MethodPut:
 		w.Write([]byte("Hello Put Execs Route"))
-		fmt.Println("Hello Put Execs Route")
 		return
 	case http.MethodPatch:
 		w.Write([]byte("Hello Patch Execs Route"))
-		fmt.Println("Hello Patch Execs Route")
 		return
 	case http.MethodDelete:
 		w.Write([]byte("Hello Delete Execs Route"))
-		fmt.Println("Hello Delete Execs Route")
 		return
 	}
-	w.Write([]byte("Hello Execs Route"))
-	fmt.Println("Hello Execs Route")
 }
 
 func main() {
@@ -125,11 +111,18 @@ func main() {
 
 	rl := mw.NewRateLimiter(5, time.Minute)
 
+	hppOPtions := mw.HppOptions{
+		CheckQuery:                  true,
+		CheckBody:                   true,
+		CheckBodyOnlyForContentType: "application/x-www.form-urlencoded",
+		Whitelist:                   []string{"sortBy", "sortOrder", "name", "age", "class"},
+	}
+
 	server := &http.Server{
 		Addr: port,
-		Handler: rl.Middleware(
+		Handler: mw.Hpp(hppOPtions)(rl.Middleware(
 			mw.Compression(mw.ResponseTimeMiddleware(mw.SecurityHeaders(mw.Cors(mux)))),
-		),
+		)),
 		TLSConfig: tlsConfig,
 	}
 
---
+++ rest-api-project/REST_API_GO/internal/api/middlewares/hpp.go
@@ -0,0 +1,72 @@
+package middlewares
+
+import (
+	"fmt"
+	"net/http"
+	"slices"
+	"strings"
+)
+
+type HppOptions struct {
+	CheckQuery                  bool
+	CheckBody                   bool
+	CheckBodyOnlyForContentType string
+	Whitelist                   []string
+}
+
+func Hpp(options HppOptions) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if options.CheckBody && r.Method == http.MethodPost &&
+				isCorrectContentType(r, options.CheckBodyOnlyForContentType) {
+				filterBodyParams(r, options.Whitelist)
+			}
+			if options.CheckQuery && r.URL.Query() != nil {
+				filterQueryParams(r, options.Whitelist)
+			}
+			next.ServeHTTP(w, r)
+		})
+	}
+}
+
+func isCorrectContentType(r *http.Request, contentType string) bool {
+	return strings.Contains(r.Header.Get("Content-Type"), contentType)
+}
+
+func filterBodyParams(r *http.Request, whiteList []string) {
+	err := r.ParseForm()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for k, v := range r.Form {
+		if len(v) > 1 {
+			r.Form.Set(k, v[0])
+			// r.Form.Set(k, v[len(v)-1])
+		}
+		if !isWhiteListed(k, whiteList) {
+			delete(r.Form, k)
+		}
+	}
+}
+
+func filterQueryParams(r *http.Request, whiteList []string) {
+	query := r.URL.Query()
+
+	for k, v := range query {
+		if len(v) > 1 {
+			// query.Set(k, v[0])
+			query.Set(k, v[len(v)-1])
+		}
+		if !isWhiteListed(k, whiteList) {
+			delete(query, k)
+		}
+	}
+
+	r.URL.RawQuery = query.Encode()
+}
+
+func isWhiteListed(param string, whiteList []string) bool {
+	return slices.Contains(whiteList, param)
+}
